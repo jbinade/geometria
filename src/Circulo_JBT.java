bc0f1880dde7c5a109a9c426bba5384e0dc34a63
Jesus <94246161+jbinade@users.noreply.github.com>
2023-02-20 20:47:38 +0100

    versión refactorizada

--- src/Circulo_JBT.java
+++ src/Circulo_JBT.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JBT extends FiguraGeometrica_JBT {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JBT(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- src/Rectangulo_JBT.java
+++ src/Rectangulo_JBT.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_JBT extends FiguraGeometrica_JBT {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_JBT(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
