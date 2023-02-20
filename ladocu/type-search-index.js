187bbed666db9fc9593fd217db36a9efbf9ff048
Jesus <94246161+jbinade@users.noreply.github.com>
2023-02-20 21:15:40 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_JBT"},{"p":"<Unnamed>","l":"FiguraGeometrica_JBT"},{"p":"<Unnamed>","l":"Rectangulo_JBT"},{"p":"<Unnamed>","l":"Triangulo_JBT"}];updateSearchResults();
